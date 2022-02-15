717a20816480e1620a6a4cb8574bf7e45ea25f51
Emeka Orji <emekapraiseo@gmail.com>
2022-02-16 00:12:38 +0100

    Finished the app

--- js/layout.js
+++ js/layout.js
@@ -5,16 +5,42 @@ speechText.onclick = () => {
   input.focus();
 }
 
-var theme = document.querySelector(".message");
-theme.onclick = () => {
+var navBar = document.querySelector(".nav");
+var navItems = document.querySelectorAll(".nav_item:not(#profile)");
+var profile = document.getElementById("profile");
+var theme = document.getElementById("themeSwitch");
+
+
+profile.addEventListener("click", () => {
+  navBar.classList.toggle("nav_expanded");
+});
+
+
+// profile.addEventListener("blur", () => {
+//   profile.focus();
+// });
+// navItems.forEach(e => e.addEventListener("click", () => {
+//   console.log("profile is focused");
+//   if (document.activeElement === profile) {
+//     profile.focus();
+//   }
+// }));
+
+
+
+
+
+var theme = [document.getElementById("themeButton"), document.getElementById("themeButtonPC")];
+theme.forEach(e => e.onclick = () => {
   document.body.classList.toggle("darkmode");
-}
+});
 window.onload = () => {
   document.body.classList.toggle("darkmode");
 }
 
-var historyIcon = document.getElementById("historyIcon");
+const history = document.querySelector(".history_header");
 var mainContent = document.querySelector(".main-content");
-historyIcon.onclick = () => {
-  mainContent.classList.toggle("history-expanded");
-}
+history.addEventListener("click", () => {
+  mainContent.classList.toggle("history_expanded");
+  history.parentElement.classList.toggle("history_active")
+});
