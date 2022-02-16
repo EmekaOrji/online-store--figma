f30544109fdfbab660a6c4ad57e5acccc3de1810
Emeka Orji <emekapraiseo@gmail.com>
2022-02-16 01:01:40 +0100

    Added some touches, and ready to merge

--- js/layout.js
+++ js/layout.js
@@ -1,45 +1,43 @@
 var speechText = document.getElementById("speechText");
 var input = document.querySelector("input");
+var navBar = document.querySelector(".nav");
+var navItems = document.querySelectorAll(".nav_item:not(#profile)");
+var profile = document.getElementById("profile");
+var mainContent = document.querySelector(".main-content");
+var mic = document.getElementById("speechIcon");
+var theme = [document.getElementById("themeButton"), document.getElementById("themeButtonPC")];
+const history = document.querySelector(".history_header");
 
 speechText.onclick = () => {
   input.focus();
 }
-
-var navBar = document.querySelector(".nav");
-var navItems = document.querySelectorAll(".nav_item:not(#profile)");
-var profile = document.getElementById("profile");
-var theme = document.getElementById("themeSwitch");
-
+mic.onclick = activateMic;
+mic.onblur = deActivateMic;
+document.onkeydown = (e) => {
+  e = e || window.event;
+  var key = e.which || e.keyCode;
+  if(key === 83){
+      activateMic();
+  }
+}
+function activateMic() {
+  mic.classList.toggle("mic_active");
+}
+function deActivateMic() {
+  mic.classList.remove("mic_active");
+}
 
 profile.addEventListener("click", () => {
   navBar.classList.toggle("nav_expanded");
 });
 
-
-// profile.addEventListener("blur", () => {
-//   profile.focus();
-// });
-// navItems.forEach(e => e.addEventListener("click", () => {
-//   console.log("profile is focused");
-//   if (document.activeElement === profile) {
-//     profile.focus();
-//   }
-// }));
-
-
-
-
-
-var theme = [document.getElementById("themeButton"), document.getElementById("themeButtonPC")];
 theme.forEach(e => e.onclick = () => {
   document.body.classList.toggle("darkmode");
 });
-window.onload = () => {
-  document.body.classList.toggle("darkmode");
-}
+// window.onload = () => {
+//   document.body.classList.toggle("darkmode");
+// }
 
-const history = document.querySelector(".history_header");
-var mainContent = document.querySelector(".main-content");
 history.addEventListener("click", () => {
   mainContent.classList.toggle("history_expanded");
   history.parentElement.classList.toggle("history_active")
